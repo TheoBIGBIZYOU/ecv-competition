e60fc933344a4777d42c1d4809a83a1c2e50ab57
theoBIGBIZYOU <theo@bigbizyou.agency>
2023-06-20 10:26:30 +0200

    add font

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import QuestionnaireScreen from "./views/QuestionnaireScreen.vue";
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 :root {
   --blue: #3b63db;
   --green: #69d38c;
@@ -28,6 +29,7 @@ body {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+  font-family: 'Work Sans', sans-serif;
 }
 
 .flex {
